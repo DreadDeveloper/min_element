480869297df3f3afe86ee3fe8ce4b3cf116f7d04
Abhinav Kumar <kumaraka2000@gmail.com>
2024-01-02 19:12:58 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {25, 5, 67, 89, 55};
+        int min = arr[0];
+        int minIdx = -1;
+        int max = arr[0];
+        int maxIdx = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                minIdx = i;
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIdx = i;
+            }
+        }
+        System.out.println(minIdx + " " + maxIdx);
+        arr[minIdx] = arr[0];
+        arr[0] = min;
+        arr[maxIdx] = arr[arr.length - 1];
+        arr[arr.length - 1] = max;
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
